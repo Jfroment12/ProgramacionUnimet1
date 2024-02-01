e60e16461c3d3d02d12177e0a3c228f7e22907a1
Jfroment12 <148997948+Jfroment12@users.noreply.github.com>
2024-02-01 17:13:40 -0400

    Arepas en VisualCode_Python
    Profesor adjunto mi ejercicio relacionado a ingredientes de las arepas

---
+++ ArepasJavFro.py
@@ -0,0 +1,5 @@
+Masa=int(input("Que cantidad de masa quiere utilizar?"))
+Sal=float(input("Que cantidad de sal quiere utilziar?"))
+Agua=int(input("Que cantidad de agua quiere utilizar?"))
+print("Para hacer una arepa se necesita", Masa, "gr de masa ,", Sal, "pizcas de sal ,", Agua, "litros de agua .")
+             
